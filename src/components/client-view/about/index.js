6c5eac6df0cac1d61703304b51aceaf502c3e05f
codewithsmile01 <mdismailismail1034@gmail.com>
2024-12-09 22:39:45 +0600

    4th commit

--- src/components/client-view/about/index.js
+++ src/components/client-view/about/index.js
@@ -51,10 +51,13 @@ export default function ClientAboutView({ data }) {
     },
   ];
 
-  const headingText = "Why Hire Me For Your Next Project ?";
+  const headingText = "Why Hire Me For Next Project ?";
 
   return (
-    <div className="max-w-screen-xl mt-24 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto" id="about">
+    <div
+      className="max-w-screen-xl mt-24 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
+      id="about"
+    >
       <div className="w-full flex">
         <AnimationWrapper className="rounded-lg w-full grid-flow-row grid grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-green-main bg-ehite-500 z-10">
           {aboutDataInfo.map((infoItem, index) => (
