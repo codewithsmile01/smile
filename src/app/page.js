86c84f4669f1e2119ffa81f15a12af0a00677fc9
codewithsmile01 <mdismailismail1034@gmail.com>
2024-12-09 22:31:38 +0600

    3rd commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,41 @@
-import Image from "next/image";
+import ClientAboutView from "@/components/client-view/about";
+import ClientContactView from "@/components/client-view/contact";
+import ClientExperienceAndEducationView from "@/components/client-view/experience";
+import ClientHomeView from "@/components/client-view/home";
+import ClientProjectView from "@/components/client-view/project";
 
-export default function Home() {
+async function extractAllDatas(currentSection) {
+  const res = await fetch(`http://localhost:3000/api/${currentSection}/get`, {
+    method: "GET",
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+
+  return data && data.data;
+}
+
+export default async function Home() {
+  const homeSectionData = await extractAllDatas("home");
+  const aboutSectionData = await extractAllDatas("about");
+  const experienceSectionData = await extractAllDatas("experience");
+  const educationSectionData = await extractAllDatas("education");
+  const projectSectionData = await extractAllDatas("project");
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>hello world</h1>
+    <div>
+      <ClientHomeView data={homeSectionData} />
+      <ClientAboutView
+        data={
+          aboutSectionData && aboutSectionData.length ? aboutSectionData[0] : []
+        }
+      />
+      <ClientExperienceAndEducationView
+        educationData={educationSectionData}
+        experienceData={experienceSectionData}
+      />
+      <ClientProjectView data={projectSectionData} />
+      <ClientContactView />
     </div>
   );
 }
