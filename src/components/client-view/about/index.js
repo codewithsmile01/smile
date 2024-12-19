1d94bd0c37d992f39be8fbcfde2dc0abef3482bd
codewithsmile01 <mdismailismail1034@gmail.com>
2024-12-19 16:56:28 +0600

    just code but not setup

--- src/components/client-view/about/index.js
+++ src/components/client-view/about/index.js
@@ -51,13 +51,47 @@ export default function ClientAboutView({ data }) {
     },
   ];
 
-  const headingText = "Why Hire Me For Next Project ?";
+  const headingText = "Why Hire Me For your Next Project ?";
+  
+  import { FocusCards } from "@/components/ui/focus-cards";
+
+  const cards = [
+    {
+      title: "8 project ",
+      src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "28 client",
+      src: "https://images.unsplash.com/photo-1600271772470-bd22a42787b3?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "3 years experience",
+      src: "https://images.unsplash.com/photo-1505142468610-359e7d316be0?q=80&w=3070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
+
 
   return (
     <div
       className="max-w-screen-xl mt-24 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
       id="about"
     >
+       <AnimationWrapper className={"pt-6"}>
+        <div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1">
+          <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-medium">
+            {headingText.split(" ").map((item, index) => (
+              <span
+                key={index}
+                className={`${index === 6 ? "text-green-600" : "text-[#000]"}`}
+              >
+                {item}{" "}
+              </span>
+            ))}
+          </h1>
+          <p className="text-[#000] mt-4 mb-8 font-bold">{data?.aboutme}</p>
+        </div>
+      </AnimationWrapper>
+      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8">
       <div className="w-full flex">
         <AnimationWrapper className="rounded-lg w-full grid-flow-row grid grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-green-main bg-ehite-500 z-10">
           {aboutDataInfo.map((infoItem, index) => (
@@ -77,34 +111,14 @@ export default function ClientAboutView({ data }) {
             >
               <div className="flex m-0 w-40 sm:w-auto">
                 <div className="flex flex-col">
-                  <p className="text-[50px] text-green-main font-bold">
-                    {infoItem.value}+
-                  </p>
-                  <p className="text-[25px] font-bold text-[#000000]">
-                    {infoItem.label}
-                  </p>
+                  <FocusCards cards={cards} />;
                 </div>
               </div>
             </motion.div>
           ))}
         </AnimationWrapper>
       </div>
-      <AnimationWrapper className={"pt-6"}>
-        <div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1">
-          <h1 className="leading-[70px] mb-4 text-3xl lg:text-4xl xl:text-5xl font-medium">
-            {headingText.split(" ").map((item, index) => (
-              <span
-                key={index}
-                className={`${index === 6 ? "text-green-600" : "text-[#000]"}`}
-              >
-                {item}{" "}
-              </span>
-            ))}
-          </h1>
-          <p className="text-[#000] mt-4 mb-8 font-bold">{data?.aboutme}</p>
-        </div>
-      </AnimationWrapper>
-      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8">
+   
         <AnimationWrapper className="flex w-full">
           <motion.div variants={setVariants} className="h-full w-full p-4">
             <Image
